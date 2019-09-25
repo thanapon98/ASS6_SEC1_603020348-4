b1b9fe0ab96d38d5b964096ff5a78a6c9b3fa93a
thanapon98 <s.thanapon@kkumail.com>
2019-09-25 21:27:16 +0700

    ASS6_SEC1_603020348-4

---
+++ app/src/main/java/com/example/admin/ass06/OneFragment.kt
@@ -0,0 +1,26 @@
+package com.example.admin.ass06
+
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import com.example.admin.ass06.R
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class OneFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_one, container, false)
+    }
+
+
+}
---
+++ app/src/main/java/com/example/admin/ass06/ThreeFragment.kt
@@ -0,0 +1,38 @@
+package com.example.admin.ass06
+
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import com.example.admin.ass06.R
+import kotlinx.android.synthetic.main.fragment_two.view.*
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class ThreeFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_three, container, false)
+        view.btnFragTwo.setOnClickListener(){
+            var fragment: Fragment? = null
+            fragment = OneFragment()
+            replaceFragment(fragment)
+        }
+
+        return view
+    }
+    fun replaceFragment(someFragment: Fragment){
+        val transaction = fragmentManager!!.beginTransaction()
+        transaction.replace(R.id.frameLayout,someFragment)
+        transaction.addToBackStack(null)
+        transaction.commit()
+    }
+
+}
